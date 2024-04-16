279c212ac8ad7a3c2c7be50a1db146909be29062
Ankita Nadarge <146661081+nadargeAnkita@users.noreply.github.com>
2024-04-16 22:24:34 +0530

    Add files via upload

---
+++ #54 javascript intro & Node.Js installation/script.js
@@ -0,0 +1,15 @@
+alert("Hello World");
+
+console.log("Code is running...")
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure you want to leave this page")
+
+if(isTrue){
+    console.log("Computer is blasting")
+}
+else{
+    console.log("Computer is not blasting")
+}
+
+console.log("Your number is" + a)
