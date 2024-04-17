75bd1b5c02480e5d0a6d94383db11314fa9223f3
Ankita Nadarge <146661081+nadargeAnkita@users.noreply.github.com>
2024-04-17 20:05:42 +0530

    Add files via upload

---
+++ #55 javaScript variables, data types, objects/script.js
@@ -0,0 +1,49 @@
+console.log("hey this is tutorial 55")
+
+let a = 5;
+a = a + 1
+let b = 6;
+let c = "Ankita";
+let _a = "Ishwar";
+// var 55a = "Srushti "              //  NOT ALLOWED
+
+// console.log(a+b)
+// console.log(typeof a, typeof b, typeof c)
+
+
+
+{
+    let a = 66;
+    console.log(a)
+}
+console.log(a)
+
+// const a1 = 6;
+// a1 = a1 + 6;                   //   NOT ALLOWED BCOZ a1 IS CONSTANT
+
+
+
+
+
+let x = "ankita";
+let y = 32;
+let z = 7.33;
+const p = true;
+let q = undefined;
+let r = null;
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+
+
+
+let o = {
+    "name": "ANKITA",
+    "job code": 32, 
+    "is handsome": true
+}
+console.log(o)
+o.salary = "100k"
+console.log(o)
+o.salary = "500k"
+console.log(o)
