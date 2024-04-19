bc5213872e6b54184246c356ed725be09aeada91
Ankita Nadarge <146661081+nadargeAnkita@users.noreply.github.com>
2024-04-19 19:28:36 +0530

    Add files via upload

---
+++ #57 JavaScriptLoop/index.js
@@ -0,0 +1,37 @@
+console.log("I am a tutorial on loops")
+
+// let a = 1;
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+// }
+
+// let obj = {
+//     name: "Ankita",
+//     role: "Programmer",
+//     company: "Reddy"
+// }
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key, element)
+// }
+
+// for (const c of "Ankita") {
+//     console.log(c)
+// }
+
+// let i = 0;
+// while (i<6) {
+//     console.log(i)
+//     i++;
+// }
+
+let i = 10;
+do {
+    console.log(i)
+    i++;
+    
+} while (i<6);
+
+
