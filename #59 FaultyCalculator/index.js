5fac665d93a30bb82b7e31c01f8dcf063e92f5e3
Ankita Nadarge <146661081+nadargeAnkita@users.noreply.github.com>
2024-04-22 18:36:40 +0530

    Add files via upload

---
+++ #59 FaultyCalculator/index.js
@@ -0,0 +1,14 @@
+/*  Create a Faulty Calculator using Javascript
+
+The faulty calculator does following:
+1. It takes two Numbers as input from the user
+2. It performs wrong operations as follow:
++ ---> -
+* ---> +
+- ---> /
+/ ---> **  
+
+
+It performs wrong operations 10% of the times
+
+*/
