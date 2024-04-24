718667bf33e7293cefe5cb9b0be6afff90538623
Ankita Nadarge <146661081+nadargeAnkita@users.noreply.github.com>
2024-04-24 22:10:57 +0530

    Add files via upload

---
+++ #60 Javascript Strings/index.js
@@ -0,0 +1,30 @@
+console.log("This is Strings Tutorial");
+
+let a = "ANKITA";
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+// console.log(a[6]);
+
+console.log(a.length)
+
+let real_name = "Ankita";
+let friend = "Vaidehi";
+console.log("Her name is " + real_name + " and her friends name is " + friend)
+console.log(`My name is ${real_name} and my friend name is ${friend} `)
+
+let c = "Radha";
+console.log(c.toUpperCase());
+console.log(c.toLowerCase());
+console.log(c.slice(1, 3));
+console.log(c.slice(1));
+console.log(c.replace("Ra", "An"));
+console.log(c.concat(a, "Srushti"));
+console.log(a);
+
+
+
